f6da6003d3e610f31b6979dd537216a44032c0c4
Amadeo de Gracia <amadeodeg@gmail.com>
2015-05-10 18:05:47 +0200

    Primeros pasos base de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,16 @@
 exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().succes(function(quiz) {
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	})
 };
 
 
 exports.answer = function (req, res) {
-	if (req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+	models.Quiz.findAll().succes(function(quiz) {
+		if (req.query.respuesta === quiz[0].respuesta){
+			res.render('quizes/answer', {respuesta: 'Correcto'});
+		} else {
+			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		}
+	})	
 };
---
+++ models/models.js
@@ -0,0 +1,38 @@
+var path =require('path');
+
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name	 = (url[6]||null);
+var user	 = (url[2]||null);
+var pwd		 = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect	 = (url[1]||null);
+var port 	 = (url[5]||null);
+var host	 = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(DB_name, user, pwd,
+					{dialect: protocol,
+					 protocol: protocol,
+					 port: port,
+					 host: host,
+					 storage: storage,
+					 omitNull: true}
+					);
+
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz;
+
+sequelize.sync().success(function() {
+	Quiz.count().success(function (count){
+		if(count === 0) {
+			Quiz.create({ pregunta: 'Capital de Italia',
+							respuesta: 'Roma'
+						})
+			.success(function(){console.log('Base de datos inicializada')});
+		};
+	});
+});
