e9bec895cc0accaee75a87766c72edc81f2e0459
amadeo <amadeodeg@gmail.com>
2015-05-18 01:09:44 +0200

    Cualquiera puede añadir preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -49,6 +49,22 @@ exports.answer = function (req, res) {
 };
 
 
+exports.new = function (req, res) {
+	var	quiz = models.Quiz.build(
+			{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+
+exports.create = function (req, res){
+	console.log("hasta aqui");
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})
+};
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,8 @@ router.param('quizId', quizController.load);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-//router.get('/quizes/new', quizController.new);
-//router.get('quizes/create', quizController.create);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 router.get('/author', function(req,res){
 	res.render('author');
