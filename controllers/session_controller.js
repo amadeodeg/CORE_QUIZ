636223d39e3bf3157acc53ba5ef89f6f1163e5a1
amadeo <amadeodeg@gmail.com>
2015-05-20 00:15:13 +0200

    Autorizacion de usuarios

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+exports.loginRequired = function(req, res, next){
+	if(req.session.user){
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
+
 exports.new = function (req, res) {
 	var errors = req.session.errors || {};
 	req.session.errors = {};
