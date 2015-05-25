9a8c4d22f89c7dae9f39f11994b17883b5525c24
amadeo <amadeodeg@gmail.com>
2015-05-25 15:48:18 +0200

    Control de publicacion de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,18 @@
 var models = require('../models/models.js');
 
+exports.load = function(req, res, next, commentId){
+	models.Comment.find({
+		where: {
+			id: Number(commentId)
+		}
+	}).then(function(comment) {
+		if(comment) {
+			req.comment = comment;
+			next();
+		} else { next(new Error('No existe commentId=' + commentId))}
+	}).catch(function(error){next(error)});
+};
+
 exports.new = function (req, res) {
 	res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
 };
@@ -22,4 +35,13 @@ exports.create = function (req, res) {
 				.then( function() { res.redirect('/quizes/'+req.params.quizId)})
 			}
 		}).catch(function(error){next(error)});
+};
+
+exports.publish = function(req, res){
+	console.log("me he muerto ya?");
+	req.comment.publicado = true;
+
+	req.comment.save( {fields:["publicado"]})
+		.then( function(){ res.redirect('/quizes/'+req.params.quizId);})
+		.catch(function(error){next(error)});
 };
--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/', function(req, res) {
 
 
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 router.get('/login',		sessionController.new);
 router.post('/login',		sessionController.create);
@@ -35,6 +36,7 @@ router.delete('/quizes/:quizId(\\d+)' 		,sessionController.loginRequired,		quizC
 
 router.get('/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', 		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/quizes/statistics'			,statisticsController.showAll);
 
